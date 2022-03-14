d7d0873cc2e9ea2cd5d31944e329250c7475a253
CharlieFunk1 <ydougroove@gmail.com>
2022-03-14 13:30:50 -0700

    complicated

--- app/models.py
+++ app/models.py
@@ -18,6 +18,26 @@ class Strip(db.Model):
     def __repr__(self):
         return '<Strip Number {}>'.format(self.strip_num)
 
+class StripSnake(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    strip_num = db.Column(db.Integer, index=True, unique=True)
+    strip_segments = db.Column(db.Integer)
+    start_pos_x = db.Column(db.Integer)
+    start_pos_y = db.Column(db.Integer)
+    line_color_r = db.Column(db.Integer)
+    line_color_g = db.Column(db.Integer)
+    line_color_b = db.Column(db.Integer)
+    ip = db.Column(db.String(15), index=True, unique=True)
+    segments = db.relationship('StripSnakeSegment', backref='strip_num', lazy='dynamic')
+
+class StripSnakeSegment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    segment_number = db.Column(db.Integer, index=True)
+    num_pixels = db.Column(db.Integer)
+    angle = db.Column(db.Integer)
+    length = db.Column(db.Integer)
+    segment_strip_num = db.Column(db.Integer, db.ForeignKey('stripsnake.strip_num'))
+
 class Configure(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     num_strips = db.Column(db.Integer, index=True)
