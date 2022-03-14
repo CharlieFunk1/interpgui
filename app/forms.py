c4539c41986991030fac764e022f0ffdd2c064b3
CharlieFunk1 <ydougroove@gmail.com>
2022-03-14 13:34:28 -0700

    Revert "complicated"
    This reverts commit d7d0873cc2e9ea2cd5d31944e329250c7475a253.

--- app/forms.py
+++ app/forms.py
@@ -18,25 +18,6 @@ class StripForm(FlaskForm):
     ip = StringField('IP address of Strip', validators=[IPAddress(message="Value needs to be a valid IP Address")])
     submit = SubmitField('Submit Values')
     
-
-class StripFormSnakeGeneral(FlaskForm):
-    strip_num = IntegerField('Strip Number', validators=[DataRequired(), NumberRange(min=1, max=None, message="There is no strip 0.  Must start with 1.")])
-    strip_segments = IntegerField('Number of Segemnts', validators=[DataRequired(), NumberRange(min=1, max=None, message="Can not have 0 segments")])
-    start_pos_x = IntegerField('Start Position X Value', validators=[DataRequired()])
-    start_pos_y = IntegerField('Start Position Y Value', validators=[DataRequired()])
-    line_color_r = IntegerField('Strip Color Red (0-255)', validators=[NumberRange(min=0, max=255, message="Value needs to be 0-255")])
-    line_color_g = IntegerField('Strip Color Green (0-255)', validators=[NumberRange(min=0, max=255, message="Value needs to be 0-255")])
-    line_color_b = IntegerField('Strip Color Blue (0-255)', validators=[NumberRange(min=0, max=255, message="Value needs to be 0-255")])
-    ip = StringField('IP address of Strip', validators=[IPAddress(message="Value needs to be a valid IP Address")])
-    submit = SubmitField('Submit Values')
-
-class StripFormSnakeSegment(FlaskForm):
-    num_pixels = IntegerField('Number of Pixels in Strip', validators=[DataRequired(), NumberRange(min=1, max=300, message="Must be 1-300")])
-    angle = IntegerField('Angle (In Degrees)', validators=[NumberRange(min=-359, max=359, message="Must be between -359 and 359 degrees")])
-    length = IntegerField('Length (in Pixels)', validators=[DataRequired()])
-    submit = SubmitField('Submit Values')
-    
-    
 class ConfigForm(FlaskForm):
     num_strips = IntegerField('Number of Strips', validators=[DataRequired(), NumberRange(min=1, max=None, message="Can not have 0 strips")])
     rust_path = StringField('Path to Program (/home/path/to/rust/program/)', validators=[DataRequired()])
